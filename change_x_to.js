e0be798866948a5e3fa1118f4d406960b9fca761
oriverk <oriveryk@gmail.com>
2023-08-01 18:46:39 +0900

    refactor

--- change_x_to.js
+++ change_x_to.js
@@ -1,31 +1,37 @@
-const img = chrome.runtime.getURL("/icons/image.png");
+const src = chrome.runtime.getURL("/icons/image.png");
 
-const checkTwitterIcons = () => {
-  const heading = document.querySelector("h1[role='heading'] svg");
-
-  if (headingIcon) {
-    const parent = headingIcon.parentElement;
-    heading.remove();
-    const newIcon = document.createElement("img");
-    newIcon.setAttribute("src", img);
-    newIcon.setAttribute("style", "width:36px;");
-    parent.appendChild(newIcon);
-  }
-}
-
-/**
- * Change Favicon
- * thanks for http://0rbit.cybergence.net/torimodosu.html
- */
-function changeFaviconHref() {
+window.addEventListener('load', () => {
   const link = document.querySelector('link[rel="shortcut icon"]');
   if (link) {
-    link.setAttribute('href', img);
+    link.setAttribute('href', src);
   }
+
+  waitForSelector('#react-root header h1[role="heading"] svg').then((logo) => {
+    if (!logo) return;
+    logo.outerHTML = `<img src="${src}">`;
+  });
+
+  // Splash Logo
+  // waitForSelector('#placeholder > svg').then((splash) => {
+  //   if (!splash) return;
+  //   splash.outerHTML = `<img src="${src}">`;
+  // });
+});
+
+function waitForSelector(selector) {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) return resolve(document.querySelector(selector));
+
+    const observer = new MutationObserver(() => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document, {
+      childList: true,
+      subtree: true
+    });
+  });
 }
-
-// Change Favicon
-window.addEventListener('load', changeFaviconHref);
-
-// DOM監視
-timer = setInterval(checkTwitterIcons, 100);
